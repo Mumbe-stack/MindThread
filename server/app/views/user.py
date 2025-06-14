1375a709e1ea0d31b2cf9a4077716ce5407bdca7
Mercy <mercymumbe17@gmail.com>
2025-06-14 23:13:36 +0300

    added user operations

---
+++ server/app/views/user.py
@@ -0,0 +1,10 @@
+from flask import Blueprint,jsonify
+
+user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/<int:user_id>', methods=['GET'])
+
+def profile():
+    return jsonify ({
+        "success" : "User profile endpoint works"
+    })
