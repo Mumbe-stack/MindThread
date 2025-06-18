b747c022b2eed095bda5cd49d8aaa0410e49dd3a
Mercy <mercymumbe17@gmail.com>
2025-06-18 19:08:04 +0300

    Restructure: remove old flat layout, add backend and frontend folders

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'app.db')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "jwt_secret_key"
--- views/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-home_bp = Blueprint('home_bp', __name__)
-
-@home_bp.route("/")
-def index():
-    return jsonify({"message": "Welcome to MindThread API"})
-
